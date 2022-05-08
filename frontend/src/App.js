d25d2e31b4962ac9c1ab4016c69e2beb3afcd57d
Joshua <joshua.wu123@gmail.com>
2022-05-07 19:57:15 -0500

    get url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,47 @@
+/* global chrome */
 import logo from './logo.svg';
 import './App.css';
 import React, {useState, useEffect} from 'react';
+export const App = () => {
+  const [url, setUrl] = useState('');
+  const [result, setResult] = useState('');
+  useEffect(() => {
+      const queryInfo = {active: true, lastFocusedWindow: true};
 
-function App() {
-  const [result, setResult] = useState("Defalut");
+      chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+          const url = tabs[0].url;
+          setUrl(url);
+      });
+  }, []);
+
+
   function getResult() {
+
+    return fetch("http://127.0.0.1:5000/backend/classifier").then(response => response.json()
+    .then(data => {setResult(data.result);}));
+  }
+  return (
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>URL:</p>
+            <p>
+                {url}
+            </p>
+            <button onClick={getResult}>Get Result</button>
+            <p>Result:</p>
+            <p>
+                {result}
+            </p>
+        </header>
+    </div>
+);
+};
+/*
+function App() {
+  const [result, setResult] = useState("Default");
+  function getResult() {
+
     return fetch("http://127.0.0.1:5000/backend/classifier").then(response => response.json()
     .then(data => {setResult(data.result);}));
   }
@@ -17,6 +54,5 @@ function App() {
       </header>
     </div>
   );
-}
-
+}*/
 export default App;
