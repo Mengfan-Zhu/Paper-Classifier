770577c0426176eb237cdb52288eaae11ded6714
Xinhang Chen <xinhang2@illinois.edu>
2022-05-10 17:31:23 -0500

    fix issue

--- backend/core/lda/crawler/pdfextract.py
+++ backend/core/lda/crawler/pdfextract.py
@@ -6,7 +6,7 @@ from pdfminer.high_level import extract_text
 from gensim.parsing.preprocessing import remove_stopwords, preprocess_string
 from gensim.parsing.preprocessing import preprocess_string, remove_stopwords, stem_text
 import gensim
-
+import re
 def getContentPDF(url):
     r = requests.get(url)
     f = io.BytesIO(r.content)
@@ -27,11 +27,14 @@ def getContentPDF_pdfminer(url):
     r = requests.get(url)
     f = io.BytesIO(r.content)
     text = extract_text(f)
-    text = text.replace('\n',' ')
-    print(text.encode())
-    # # tokens = gensim.utils.tokenize(text, lower=True)
-    # CUSTOM_FILTERS = [remove_stopwords, stem_text]
-    # tokens = preprocess_string(" ".join(tokens), CUSTOM_FILTERS)
-    contents = preprocess_string(text)
-    return contents
+    # print(text.encode())
+    # print(text.encode())
+    words = re.split(r'\W+', text)
+    for idx in range(len(words)):
+        words[idx] = words[idx].lower()  # Convert to lowercase.
+    # Remove numbers, but not words that contain numbers.
+    words = [ word for word in words if not word.isnumeric()]
+    # Remove words that are only one character.
+    words = [word for word in words if len(word) > 1]
+    return words
 #print(getContentPDF(url))
