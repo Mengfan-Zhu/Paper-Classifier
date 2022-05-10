267701114de6a5395ca91f803c8a0bd46d793673
TianrenZhou <tianren2@illinois.edu>
2022-05-10 12:43:21 -0700

    finishing categorizing

--- backend/core/lda/infer.py
+++ backend/core/lda/infer.py
@@ -8,16 +8,25 @@ model = LdaModel.load(model_path)
 # print the topics
 for i in range(10):
     print(model.print_topic(i))
+topics = {0:'classifier using hmm',1:'clustering for classification',2:'neuron cell study',3:'hidden layer in image recognition',4:'optimize dynamic trajectory',5:'image filter',6:'RNN model in NLP',7:'other',8:'guassian approximation',9:'mean field, boltzmann and attention mechanism'}
 url = 'https://www.researchgate.net/profile/Ferdinando-Samaria/publication/220611207_HMM-based_architecture_for_face_identification/links/5c670ed8299bf1e3a5abdff0/HMM-based-architecture-for-face-identification.pdf'
 # infer new document
 new_doc = [
     ['computer', 'time', 'graph'],
     ['survey', 'response', 'eps'],
     ['text', 'retrieval', 'advance','science','aaaa'],
+    getContentPDF(url)
 ]
 dictionary = corpora.Dictionary.load("../../../model/nips.dict")
 other_corpus = [dictionary.doc2bow(text) for text in new_doc]
 print(other_corpus)
-unseen_doc = other_corpus[2]
+unseen_doc = other_corpus[3]
 vector = model[unseen_doc]
 print(vector)
+topic = vector[0][0]
+prob = vector[0][1]
+for i in vector:
+    if i[1] > prob:
+        prob = i[1]
+        topic = i[0]
+print(topics[topic])
